922f30901511a09da442ead139bd83158798d9c8
Oliver Diamond <oliver.diamond@sky.uk>
2019-01-09 09:37:17 +0000

    allowing symbols in passwords

--- src/server/controllers/accounts.js
+++ src/server/controllers/accounts.js
@@ -5,7 +5,7 @@ const config = require('config');
 
 const usernameAndPasswordSchema = {
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{3,40}$/).required(),
 };
 
 const createAccountSchema= Joi.object().keys({
