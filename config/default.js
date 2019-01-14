52dc6366556b1bccc58be0eb6eacdd6817a97f9b
Oliver Diamond <oliver.diamond@sky.uk>
2019-01-14 11:14:20 +0000

    adding react router

---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+  db: {
+    mongo: {
+      dbName: 'auth-app',
+    },
+  },
+};
--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { SheetsRegistry } from 'jss';
 import JssProvider from 'react-jss/lib/JssProvider';
+import { StaticRouter } from 'react-router-dom';
 import Application from '../../ui/Application.jsx';
 import html from '../../ui/layout/basic.js';
 
-const title = 'Authentication';
-
 module.exports = {
   async showIndex(req, res) {
+    const title = 'Hello World Title';
     const initialState = {};
     const sheetsRegistry = new SheetsRegistry();
     const body = renderToString(
       <JssProvider registry={sheetsRegistry} >
-        <Application {...initialState} />
+        <StaticRouter location={req.url} context={{}} >
+          <Application {...initialState} />
+        </StaticRouter>
       </ JssProvider>
     );
     res.send(html({ title, body, initialState, css: sheetsRegistry.toString() }));
+  },
+  async showData(req, res) {
+    res.json({ hello: 'world' });
   },
 };
